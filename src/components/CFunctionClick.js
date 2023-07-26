6b91952c6ec1eb1819fbcd750cdf89a889cef046
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2023-07-26 09:47:33 +0530

    Initial commit

---
+++ src/components/CFunctionClick.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class CFunctionClick extends Component {
+
+ClickEvent(){
+    console.log("class button clicked")
+}
+
+  render() {
+    return (
+      <button onClick={this.ClickEvent}>click button 2</button>
+      
+    )
+  }
+}
+
+export default CFunctionClick
